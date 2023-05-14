158b5606eb9d90d8a95eae52bb747e2b7e1fcc08
Brooke Love <savienloveb@gmail.com>
2023-05-14 18:51:38 -0400

    creating a switch case

--- GROWTH/src/App.java
+++ GROWTH/src/App.java
@@ -1,5 +1,43 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to GROWTH!");
+        Scanner input = new Scanner(System.in);
+        System.out.println(
+                "Welcome to GROWTH! This is a place where you can categorize your plants as well as learn about your plants health!");
+
+        String mainMenu;
+        String response;
+        // need to use the common name vs the actual name option
+        // System.out.println("You choose: " + mainMenu);
+        do {
+            System.out.println(
+                    "Here are the options to check out your virtual greenhouse:\nA.Add a New Plant\nB.Check out Greenhouse\nC.Update a Plant\nD. Delete A Plant\nE. Get Info\nF.Exit");
+            response = input.nextLine().toUpperCase();
+            // response.toUpperCase();
+            switch (response) {
+                case "A":
+                    System.out.println("Great lets add a new plant!");
+                    break;
+                case "B":
+                    System.out.println("Onto the GreenHouse!");
+                    break;
+                case "C":
+                    System.out.println("Lets update that Plant!");
+                    break;
+                case "D":
+                    System.out.println("Lets remove one of you plants from the inventory!");
+                    break;
+                case "E":
+                    System.out.println("Lets update that Plant!");
+                    break;
+                case "F":
+                    System.out.println("Ok we will see you later!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.printf("%s is not valid please try again.", response);
+            }
+        } while (response != "F");
     }
 }
--- GROWTH/src/Plant.java
+++ GROWTH/src/Plant.java
@@ -1,7 +1,7 @@
 abstract class Plant {
     // holds all of the common attributes with plants [leaves, varigation, light,
     // water]
-    public String name;
+    public String commonName;
     private Float age;
     private String lightSource;
     public Boolean hasLeaves;
@@ -11,12 +11,11 @@ abstract class Plant {
 
     public abstract void grow();
 
-    // steps to make it grow in the optimal condition
     public void varigation() {
         // If there was varigation then the plant color needs to be said
     }
 
-    class Succulents extends Plant {
+    class Succulent extends Plant {
         // adding in succulent characteristics that make it differnent from others
         private Boolean hasSpikes;
 
@@ -27,11 +26,11 @@ abstract class Plant {
 
         @Override
         public void grow() {
-
+            // steps to make it grow in the optimal condition
         }
     }
 
-    class Flowering extends Plant {
+    class Foliage extends Plant {
         // adding in cactus characteristics that make it differnent from others
         private String bloomColor;
 
@@ -46,111 +45,4 @@ abstract class Plant {
         }
     }
 
-    class nonFlowering extends Plant {
-        // adding in nonFlowering characteristics that make it differnent from others
-        @Override
-        public void waterPlants() {
-            // differrent kind of watering type than the
-        }
-
-        @Override
-        public void grow() {
-
-        }
-    }
-
-    class Ferns extends Plant {
-        // adding in ferns characteristics that make it differnent from others
-        @Override
-        public void waterPlants() {
-            // differrent kind of watering type than the
-        }
-
-        @Override
-        public void grow() {
-
-        }
-    }
-
-    class Mosses extends Plant {
-        // adding in mosses characteristics that make it differnent from others
-        // private Boolean hasGrowths;
-        @Override
-        public void waterPlants() {
-            // differrent kind of watering type than the
-        }
-
-        @Override
-        public void grow() {
-
-        }
-    }
-
-    class Herbs extends Plant {
-        // adding in succulent characteristics that make it differnent from others
-        @Override
-        public void waterPlants() {
-            // differrent kind of watering type than the
-        }
-
-        @Override
-        public void grow() {
-
-        }
-    }
-
-    class Shrubs extends Plant {
-        // adding in succulent characteristics that make it differnent from others
-        @Override
-        public void waterPlants() {
-            // differrent kind of watering type than the
-        }
-
-        @Override
-        public void grow() {
-
-        }
-    }
-
-    class Trees extends Plant {
-        // adding in trees characteristics that make it differnent from others
-        @Override
-        public void waterPlants() {
-            // differrent kind of watering type than the
-        }
-
-        @Override
-        public void grow() {
-
-        }
-    }
-
-    class Climbers extends Plant {
-        // adding in climbers characteristics that make it differnent from others
-        public Float vineLength;
-        public Float amountVines;
-
-        @Override
-        public void waterPlants() {
-            // differrent kind of watering type than the
-        }
-
-        @Override
-        public void grow() {
-
-        }
-    }
-
-    class Aquatics extends Plant {
-        // adding in aquatic characteristics that make it differnent from others
-        @Override
-        public void waterPlants() {
-            // differrent kind of watering type than the
-        }
-
-        @Override
-        public void grow() {
-
-        }
-    }
 }
