8bd84c6109b9dbed36ced1b280732171fc48bea2
brookelove <sblove@bu.edu>
2023-05-24 00:35:45 -0400

    getting working tree

--- GROWTH/src/App.java
+++ GROWTH/src/App.java
@@ -6,36 +6,28 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(
                 "Welcome to GROWTH! This is a place where you can categorize your plants as well as learn about your plants health!");
-
-        String mainMenu;
         Integer response;
-        // need to use the common name vs the actual name option
-        // System.out.println("You choose: " + mainMenu);
         do {
             System.out.println(
-                    "Here are the options to check out your virtual greenhouse:\n1.Add a New Plant\n2.Check out Greenhouse\n3.Update a Plant\n4. Delete A Plant\n5. Get Info\n6.Exit");
+                    "Here are the options to check out your virtual greenhouse:\n1.Add a New Plant\n2.Check out Greenhouse\n3.Update a Plant\n4. Delete A Plant\n5.Exit");
             response = input.nextInt();
             // response.toUpperCase();
             switch (response) {
                 case 1:
-                    System.out.println("Great lets add a new plant!");
+                    System.out.println("Add a Plant");
                     MainMenu.newPlantInfo();
                     break;
                 case 2:
-                    System.out.println("Onto the GreenHouse!");
+                    System.out.println("Enter the GreenHouse!");
                     MainMenu.getGreenHouse();
                     break;
                 case 3:
-                    System.out.println("Lets update that Plant!");
+                    System.out.println("Update Plant!");
                     break;
                 case 4:
-                    System.out.println("Lets remove one of you plants from the inventory!");
+                    System.out.println("Remove a Plant");
                     break;
                 case 5:
-                    System.out.println("Plantary");
-                    MainMenu.plantDictionary();
-                    break;
-                case 6:
                     System.out.println("Ok we will see you later!");
                     System.exit(0);
                     break;
--- GROWTH/src/MainMenu.java
+++ GROWTH/src/MainMenu.java
@@ -1,12 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.Scanner;
-//import Plant.Foliage;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class MainMenu {
     static Scanner input = new Scanner(System.in);
     static ArrayList<Foliage> foliageList = new ArrayList<Foliage>();
     static ArrayList<Succulent> succulentList = new ArrayList<Succulent>();
-
+    static ArrayList <String> choiceList = new ArrayList<String>();
+    static BufferedReader reader;
     public static void newPlantInfo() {
         String uuid;
         String commonName = null;
@@ -21,11 +28,14 @@ public class MainMenu {
         Succulent newSucculent;
 
         uuid = UUID.randomUUID().toString();
-        System.out.println("What is the Common name of your Plant?");
-        commonName = input.nextLine();
-        // need to locate the plant!
-        System.out.println("Great you've found a foliage! Lets get some information about your new plant so we can get it settled and growing!\n" +
-                "How long have you owned this plant?");
+        plantIndex();
+        System.out.println("Please choose one of the numbers below that correspond to these plants: ");
+        for (int i = 0; i < choiceList.size(); i++) {
+            System.out.printf("%s - %s\n",i+1, choiceList.get(i));
+        }
+        Integer userInput = input.nextInt();
+        commonName = choiceList.get(userInput-1);
+        System.out.println("How long have you owned this plant?");
         howLong = input.nextLine();
         System.out.println("What is the size of your pot in inches (please only use numbers)?");
         potSize = input.nextInt();
@@ -48,18 +58,16 @@ public class MainMenu {
     }
     public static void getGreenHouse() {
         Integer response;
-        // need to use the common name vs the actual name option
-        // System.out.println("You choose: " + mainMenu);
         do {
             System.out.println(
-                    "Here are the options to check out your virtual greenhouse:\n1.Succulents\n2.Foliage\n3.Plants\n4.Go Back to Main Menu");
+                    "Here are the options to check out your virtual greenhouse:\n1.Succulents\n2.Foliage\n3.Plants\n4.Plantary\n5.Go Back to Main Menu");
             response = input.nextInt();
             switch (response) {
                 case 1:
-                    System.out.printf("Total Succulents: %s\n", succulentList.size());
+                    System.out.printf("Total Succulents Added: %s\n", succulentList.size());
                     break;
                 case 2:
-                    System.out.printf("Total Foliage: %s\n", foliageList.size());
+                    System.out.printf("Total Foliage Added: %s\n", foliageList.size());
                     for (Integer i = 0; i < foliageList.size(); i ++) {
                         System.out.println("List of names:");
                         System.out.println(foliageList.get(i).getCName());
@@ -69,6 +77,10 @@ public class MainMenu {
                     System.out.printf("Total Plants: %s\n", succulentList.size() + foliageList.size());
                     break;
                 case 4:
+                    System.out.println("Your go to resource for the secrets about your plants");
+                    plantary();
+                    break;
+                case 5:
                     System.out.println("Great lets go back!");
                     return; // makes it return and go back rather than end the entire system
                 default:
@@ -76,9 +88,68 @@ public class MainMenu {
             }
         } while(response != 4);
     }
-    public static void plantDictionary(){
-        System.out.println("Welcome to Plantary! Your go to resource for the secrets of plants with our dictionary");
+    public static void plantIndex(){
+        System.out.println("Welcome to Plantary! Your go to resource for the secrets of plants with our index.\nPlease type in a letter of the common name to see the plants that start with that letter.");
+        String getGloss = input.nextLine();
+        getGloss = getGloss.toUpperCase();
+        System.out.printf("Here is that list of plants that start with the letter %s!\n", getGloss);
+        try{
+            reader = new BufferedReader(new FileReader("/Users/brookelove/code/bostonU/MET_CS_622/GROWTH/GROWTH/src/PlantDictionary.txt"));
+            String line = reader.readLine();
+            while (true) {
+                if(line == null){
+                    break;
+                }
+                line = reader.readLine();
+                if(line.contains("Common_Name:")){
+                    String splitLine [] =  line.split("Common_Name: ");
+                    String namePlant = Array.get(splitLine,1).toString();
+                    if(namePlant.startsWith(getGloss)){
+                        namePlant = namePlant.replace(",", "");
+                        choiceList.add(namePlant);
+                    }
+                }
+            }
+            reader.close();
+            System.out.println(choiceList);
+        }catch (NullPointerException e){
+            System.out.printf("");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
+    public static void plantary (){
+        //place where they can get information on their plants
+        System.out.println("Let's get some information on your plants:\nWould you like to get either 1-Foliage\n2-Succulent\n3-All. Please choose one of the three numbers given");
+        Integer plantChoiceNum = input.nextInt();
+        try{
+            reader = new BufferedReader(new FileReader("/Users/brookelove/code/bostonU/MET_CS_622/GROWTH/GROWTH/src/PlantDictionary.txt"));
+            String line = reader.readLine();
+            while (true) {
+                if(line == null){
+                    break;
+                }
+                line = reader.readLine();
+                if(plantChoiceNum.equals(1)) {
+                    //find all plants that are in the group and are equal to foliage
+                } else if (plantChoiceNum.equals(2)) {
+                    //find all plants that are in the group and are equal to succulent
+                } else {
+                    //find all plants that are in the group
+                }
+            }
+            reader.close();
+            System.out.println(choiceList);
+        }catch (NullPointerException e){
+            System.out.printf("");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
 
